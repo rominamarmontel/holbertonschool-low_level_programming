343dde8e1059d9ebbd850de2e7567180e7a2a38e
romi <romi.varnier@gmail.com>
2022-02-03 05:50:26 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * 5. Hello, printf
  */ int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
