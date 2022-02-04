79ba6ff741dd4b3c09fe8f5ee88e5d29c08913ed
romi <romi.varnier@gmail.com>
2022-02-04 12:22:14 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *main - main
+ *Return:return
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
