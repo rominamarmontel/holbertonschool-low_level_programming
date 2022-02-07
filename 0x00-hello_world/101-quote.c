dde384b2f30ad2b1f974e2d7fb20ae318940df5c
romi <romi.varnier@gmail.com>
2022-02-07 08:22:24 -0800

    9. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * 9. UNIX is basically a simple operating system, but you have to be a genius
+ * to be a genius to understand the simplicity
+ * Return: Always 0 (Success)
+ */int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
+}
