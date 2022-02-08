cff52789a42c98e75af003faf8282dc5a2c9da88
romi <romi.varnier@gmail.com>
2022-02-08 04:26:04 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,21 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 /* more headers goes there*/
 
 /**
- *main - main
+ *main - _putchar
  *Return:return
  */
 
 int main(void)
 {
-	putchar("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
