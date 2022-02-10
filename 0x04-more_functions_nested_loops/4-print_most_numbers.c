5acce2568297c373662c26fdf0ce6bb9e29030fd
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-10 21:43:47 +0100

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,14 +4,14 @@
  * Return: Always 0.
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 int i;
 
 for (i = 0; i <= 9; i++)
 {
 if(i != 2 && i != 4)
-_putchar(i);
+  _putchar(i);
 }
-_putchar('\n');
+ _putchar('\n');
 }
