02b5ae8dfd98a1f4ea269ad84793b0e2f8ecea1a
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-11 20:14:48 +0100

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *more_numbers - fonction
- *Return: rien
+ * more_numbers - fonction
+ * Return: rien
  */
 
 void more_numbers(void)
@@ -13,7 +13,7 @@ void more_numbers(void)
 	{
 		for (x = 0; x <= 14; x++)
 		{
-			if (x > 10)
+			if (x >= 10)
 				_putchar(x / 10 + '0');
 			_putchar(x % 10 + '0');
 		}
