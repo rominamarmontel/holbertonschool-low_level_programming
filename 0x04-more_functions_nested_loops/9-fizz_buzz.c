0d69ae3949c26b46a73784bb07c0d55929532701
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-11 22:17:04 +0100

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,13 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i == 100)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (i % 5 == 0)
 			printf("Buzz ");
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else
 			printf("%d ", i);
 	}
