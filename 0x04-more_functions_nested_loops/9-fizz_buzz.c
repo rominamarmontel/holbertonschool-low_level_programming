d84b2d5dba2debae1f3299968f25b6b8936c5997
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-11 22:56:19 +0100

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main - fonction
- *Return: 0
+ * main - fonction
+ * Return: 0
  */
 
 int main(void)
@@ -12,12 +12,12 @@ int main(void)
 	{
 		if (i == 100)
 			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
 	}
