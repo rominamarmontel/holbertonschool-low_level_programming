085aa63a8ac6fda745b43b231cfa50e666df1f41
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-12 17:07:20 +0100

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,17 +5,16 @@
  * @c: parametre
  * Return: Always 0.
  */
-
 int _isupper(int c)
 {
-
-	if ((c >= 65) && (c <= 90))
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
-
 	else
 	{
 		return (0);
 	}
+	_putchar('\n');
+	return (0);
 }
