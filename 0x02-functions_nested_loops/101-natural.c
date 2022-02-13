a91207efbc5c65dd6d55d730bff5b0906b8e6fd5
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-13 16:10:31 +0100

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,9 +12,9 @@ int main(void)
 	int sumx;
 	int sumi;
 
-	for (i = 0; i < 1024; i += 3)
+	for (i = 0; i <= 1024; i += 3)
 	{
-		for (i = 0; i < 1024; i += 5)
+		for (i = 0; i <= 1024; i += 5)
 			sumi += i;
 		sumx += x;
 		printf("%d\n", sumi + sumx);
