af3b84b543224bb3c049a5b097dd8b6364d038bd
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-13 22:08:54 +0100

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,11 +9,21 @@ int main(void)
 {
 	int a, b, c, i;
 
-	a = 0;
+	a = 1;
 	b = 1;
-	for (i = 0, i <= 50; i++)
-		printf("%d ,", c = a + b);
-	a = b;
-	b = c;
+	for (i = 1, i <= 50; i++)
+	{
+		printf("%d ,", a);
+	}
+	else if (i == 1)
+	{
+		printf("%d ,", b);
+	}
+	else
+	{
+		c = a + b;
+		printf("%d ,", c);
+		a = b;
+		b = c;
 	return (0);
 }
