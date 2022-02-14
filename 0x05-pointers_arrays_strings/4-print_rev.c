82b6fb09302809ece232bff9eebd7e6d3e6c35ad
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-14 16:12:09 +0100

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 /**
  * print_rev - prints a string,in reverse
  * @s: character
@@ -10,7 +9,7 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = i - 1; s[i] != '\0'; i++)
 	{
 		_putchar(s[i]);
 		_putchar('\n');
