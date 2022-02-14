53a3a4a899779cf7348ea55f608fdb4cad3e2d19
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-14 15:25:23 +0100

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_rev - prints a string,in reverse
+ * @s: character
+ * Return: rien
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	for (i = s[i]; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
