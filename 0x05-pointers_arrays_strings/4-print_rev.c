e78c93511a238a01944470450599cec0e3632895
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-14 16:30:55 +0100

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,11 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = i - 1; s[i] != '\0'; i++)
+	for (i = 0; s[i] != 0; i++)
 	{
 		_putchar(s[i]);
-		_putchar('\n');
 	}
-	for (i = i - 1; i >= 0; i--)
+	for (i = i - 1; i > 0; i--)
 	{
 		_putchar(s[i]);
 	}
