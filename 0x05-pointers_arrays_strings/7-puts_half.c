0ec5694317319449ff92deefc37566d51fdf83f8
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-14 21:49:24 +0100

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - function
+ * @str: param
+ * Return: rien
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int x = 0;
+
+	while (str[i] != 0)
+		i++;
+		x = i;
+		if (i % 2 == 0)
+			for (i = (i / 2); i != x; i++)
+			{
+				_putchar(str[i]);
+			}
+		else
+		{
+			for (i = (i + 1) / 2; i != x; i++)
+			{
+				_putchar(str[i]);
+			}
+		}
+		_putchar('\n');
+}
