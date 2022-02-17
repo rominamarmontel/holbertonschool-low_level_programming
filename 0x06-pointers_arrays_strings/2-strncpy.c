7c071feaf954befc9842ecde83924c52a69f2929
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-17 09:12:48 +0100

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * char *_strncpy - function
+ * @dest: param
+ * @src: param
+ * @n: number
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i++] = 0;
+	}
+	return (dest);
+
+}
