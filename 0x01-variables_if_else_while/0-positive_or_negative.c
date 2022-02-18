3795b229b721cf29a9a5f922b67e18af8404626a
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-18 09:32:24 +0100

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,18 +12,15 @@ int main(void)
 {
 	int n = 0;
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
-	{
 		printf("is positive\n");
-	}
 	else if (n == 0)
-	{
 		printf("is zero\n");
-	}
 	else
-	{
 		printf("is negative\n");
-	}
 	return (0);
 
 }
