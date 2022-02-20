91e385633f5117fb0b7b3b264c26bf38a48b05f6
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-20 14:00:01 +0100

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,24 +7,17 @@
 void puts_half(char *str)
 {
 	int i = 0;
-	int x = 0;
+	int last = 0;
 
-	while (str[i] != 0)
+	while (str[i] != '\0')
 	{
 		i++;
-		x = i;
 	}
-		if (i % 2 == 0)
-			for (i = (i / 2); i != x; i++)
-			{
-				_putchar(str[i]);
-			}
-		else
-		{
-			for (i = ((i + 1) / 2); i != x; i++)
-			{
-				_putchar(str[i]);
-			}
-		}
-		_putchar('\n');
+	last = (i + 1) / 2;
+	for (i = last; i <= str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
 }
