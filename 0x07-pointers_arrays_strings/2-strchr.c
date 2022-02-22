004b932acb1270e0c19b5826992c053554384c89
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-22 22:02:29 +0100

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * _strchr - locates a character in a string/find the character
  * @s: string pointer to be searched
  * @c: character to find
  * Return: the pointer of last character or NULL
  */
+
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
@@ -14,6 +16,10 @@ char *_strchr(char *s, char c)
 			return (s);
 		}
 		s++;
+	}
+		if (*s == c)
+	{
+		return (s);
 	}
 	return (0);
 }
