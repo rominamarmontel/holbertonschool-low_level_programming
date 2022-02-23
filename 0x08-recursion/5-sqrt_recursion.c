d05ae313b0d3e6d05bd1ab2b2ad941e4f192b7ad
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-23 22:44:42 +0100

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * check - checks for the square root
- * @a:int
- * @b:int
+ * sqr - checks for the square root
+ * @a: length of square
+ * @b: area of square
  *
  * Return: int
  */
-int check(int a, int b)
+
+int sqr(int a, int b)
 {
 	if (a * a == b)
 		return (a);
 	if (a * a > b)
 		return (-1);
-	return (check(a + 1, b));
+	return (sqr(a + 1, b));
 }
 
 
@@ -27,5 +28,5 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
-	return (check(1, n));
+	return (sqr(1, n));
 }
