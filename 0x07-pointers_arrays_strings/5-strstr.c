2c9d94515892a8553ebe30d7f4bcae7cd78e187c
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-23 17:57:47 +0100

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,14 +14,12 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
+		j = 0;
 		while (haystack[i] == needle[j])
 		{
 			if (haystack[i + j] == needle[j])
 				return (&haystack[i]);
 			j++;
-		}
 		}
 	}
 	return (NULL);
