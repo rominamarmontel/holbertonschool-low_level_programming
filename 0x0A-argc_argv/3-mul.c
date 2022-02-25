361bbafc3691aea9575f9fc2c757bcaa194a0a72
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-25 15:32:45 +0100

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: int
+ * @argv: string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int mul = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		mul = mul * atoi(argv[i]);
+	}
+	printf("%d\n", mul);
+
+	return (0);
+}
