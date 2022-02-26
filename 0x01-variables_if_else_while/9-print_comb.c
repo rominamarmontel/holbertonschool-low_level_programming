d32bf21c2059782680d2c2f51975dd8c65a9fc34
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-26 14:17:18 +0100

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,14 +14,12 @@ int main(void)
 
 	for (n = 48; n <= 57; n++)
 	{
+		putchar(n);
 		if (n != 57)
 		{
-			putchar(n);
 			putchar(44);
 			putchar(32);
 		}
-		else
-		putchar(n);
 	}
 	putchar('\n');
 	return (0);
