d9de1af90cd5fb19410bb5a3e99993d9cc9d3fdf
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-02-27 09:57:59 +0100

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -21,3 +21,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 #endif
+#ifndef MAIN_H
+#include "main.h"
+#endif
