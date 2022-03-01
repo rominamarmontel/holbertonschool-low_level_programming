2e99d778ff36b8327e137149cf72a98d2c07f43c
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-01 18:11:23 +0100

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - function
+ * @str: string
+ * Return: char
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate;
+	int i;
+	int size = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)/*count number of string */
+	{
+	}
+	duplicate = malloc((size + 1) * sizeof(char));/* calcul size of byte */
+	if (duplicate == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		duplicate[i] = str[i];
+	}
+	return (duplicate);
+}
