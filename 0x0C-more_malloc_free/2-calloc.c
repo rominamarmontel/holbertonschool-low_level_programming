9e03e581aa1e7e3b4eddc6383b520a2795389eac
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-03 15:37:01 +0100

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers
+ * @nmemb: param
+ * @size: param
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == 0)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)/*calloc initialize a zero all bytes*/
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
+}
