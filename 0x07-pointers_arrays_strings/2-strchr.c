c5721d528c84c78204be2fef1e3d2cb718fd6633
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-06 17:39:25 +0100

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,7 +11,7 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)/*hello wo count*/
+	for (i = 0; s[i] >= '\0'; i++)/*NULL wo fukumu hello wo count*/
 	{
 		if (s[i] == c)/*hello ni l wo dainyu*/
 			return (&s[i]);/*saisho no mojino address de return*/
