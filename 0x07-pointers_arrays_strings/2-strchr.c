7d53a6619295218b5ea39abe6a11922e02b94df1
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-06 18:20:48 +0100

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,17 +4,18 @@
  * _strchr - locates a character in a string/find the character
  * @s: string pointer to be searched
  * @c: character to find
- * Return: the pointer of last character or NULL
+ * Return: s for the pointer of first character or NULL
  */
 
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] >= '\0'; i++)/*NULL wo fukumu hello wo count*/
+	for (i = 0; s[i] >= '\0'; i++)/*NULL is considered to be part of str*/
 	{
-		if (s[i] == c)/*hello ni l wo dainyu*/
-			return (&s[i]);/*saisho no mojino address de return*/
+		if (s[i] == c)/*if c is in s*/
+			return (&s[i]);/*return a pointer to the*/
+		/* located character*/
 	}
-	return (0);
+	return (0);/*if no character NULL */
 }
