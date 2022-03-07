f6b423986eb484e99cdb5f7938f07f523d23cc85
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-07 15:54:34 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,18 +8,16 @@
  * @age: dog age/float
  * @owner: dog owner name/string
  * Description: create a new dog
- * Return: NULL if the function fails, pointer if it works
+ * Return: NULL if the function fails, pointer to new dog if it works
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i; /*dog name*/
 	int j; /*owner name*/
-	int len; /*length of a new store*/
-	dog_t *new; /*new store*/
+	int len; /*length of a new dog*/
+	dog_t *new; /*new dog*/
 
-	if (new == NULL || name == NULL || owner == NULL)
-		return (0);/*if NULL return NULL*/
 
 	for (i = 0; name[i] != '\0'; i++) /*count size of name*/
 	{
@@ -27,18 +25,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (j = 0; owner[j] != '\0'; j++) /*count size of owner*/
 	{
 	}
-	new = malloc(sizeof(dog_t)); /*allocation for new store*/
-	new->name = malloc(i); /*allocation for name of new store*/
-	new->owner = malloc(j); /*allocation for owner of new store*/
 
-	for (len = 0; name[len] != '\0'; len++)/*count name of new store*/
+	new = malloc(sizeof(dog_t)); /*allocation for new dog*/
+	if (new == NULL || name == NULL || owner == NULL)
+		return (0);/*if NULL return NULL*/
+
+	new->name = malloc(i); /*allocation for name of new dog*/
+	new->owner = malloc(j); /*allocation for owner of new dog*/
+
+	for (len = 0; name[len] != '\0'; len++)/*count name of new dog*/
 	{
-		new->name[len] = name[len];/*copy name of new store*/
+		new->name[len] = name[len];/*copy name of new dog*/
 	}
-	for (len = 0; owner[len] != '\0'; len++)/*count owner of new store*/
+	for (len = 0; owner[len] != '\0'; len++)/*count owner of new dog*/
 	{
 		new->owner[len] = owner[len];/*copy owner of new store*/
 	}
-	new->age = age;/*copy age of new store*/
+	new->age = age;/*copy age of new dog*/
 	return (new);
 }
