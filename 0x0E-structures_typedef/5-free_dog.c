139060015a96cf1d3fa72dbb5eccfe0d682b4c1f
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-07 17:02:45 +0100

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,14 +4,14 @@
 
 /**
  * free_dog - free dog
- * @d; param
+ * @d: param
  * Description: function free dog
  * Return: rien
  */
 
 void free_dog(dog_t *d)
 {
-	if(d != NULL)
+	if (d != NULL)
 	{
 		free(d->name);
 		free(d->owner);
