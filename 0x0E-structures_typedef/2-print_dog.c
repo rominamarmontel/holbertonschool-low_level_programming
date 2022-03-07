0987ee77f6e4836854bd730ece3dcc081a4462c1
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-07 14:10:20 +0100

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,10 +16,8 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
-	{
 		printf("Name: %s\n", d->name);
 		printf("Age: %.6f\n", d->age);
-	}
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
