a2d4f79be249d71a38df0305e6fdbbb4df860c28
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-09 13:27:44 +0100

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - print a name
@@ -9,6 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
+	if (name != NULL && f != NULL)
 		f(name);
 }
