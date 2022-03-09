f9fcac40197bba6b71c19d13c207307942459e30
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-09 18:55:49 +0100

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,10 +12,8 @@ int main(int argc, char *argv[])
 {
 	int num1;
 	int num2;
-	char *op;
 
 	num1 = atoi(argv[1]);
-	op = argv[2];
 	num2 = atoi(argv[3]);
 
 	if (argc != 4)
@@ -28,6 +26,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", get_op_func(op)(num1, num2));
+	printf("%d\n", get_op_func(argv[2])(num1, num2));
 	return (0);
 }
