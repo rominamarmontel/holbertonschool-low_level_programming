7ec1c00b3e4f8fba3b27e1c18f87759ca5a86159
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-09 22:20:19 +0100

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,18 +11,18 @@
 int main(int argc, char *argv[])
 {
 	int i = 1;
-	int sum = 0;
 	int j = 0;
+	int sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
+		if (argv[i][j] > 57 || argv[i][j] < 48)
+		{
+			printf("Error\n");
+			return (1);
+		}
 		}
 		sum = sum + atoi(argv[i]);
 	}
