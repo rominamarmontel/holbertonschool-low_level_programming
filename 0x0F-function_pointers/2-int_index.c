00e9c8db2b139325256724a600d667cb12addbcd
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-11 22:07:27 +0100

    1-array_iterator.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,9 +2,9 @@
 
 /**
  * int_index - search for an integer
- * @array: param
- * @size: param
- * @cmp: param
+ * @array: array
+ * @size: number of element in the array
+ * @cmp: pointer to the function/to compare
  * Return: the index of the first element for which the cmp
  */
 
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL || size < 0)
+	if (array != NULL && cmp != NULL && size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
