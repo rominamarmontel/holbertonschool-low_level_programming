49923ed86f7113877f5f3d411edbb8a80ddef602
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-11 21:20:18 +0100

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,9 +3,9 @@
 
 /**
  * array_iterator - executes a function given as a parameter on each element
- * @array: param
- * @size: param
- * @action: param
+ * @array: array
+ * @size: size of array
+ * @action: function
  * Return: rien
  */
 
@@ -13,9 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action != NULL && array != NULL && size != 0)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-			action(array[i]);
+	if (array != NULL && size != 0 && action != NULL)
+		action(array[i]);
 	}
 }
