f83f71dad66c570b175335ff322f159c2ee82cf6
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-17 21:16:19 +0100

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * add_node - add a new node
+ * @head: head of data
+ * @str: string
+ * Return: adress of the new element, NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int i;
+	list_t *newnode; /*new node*/
+
+	for (i = 0; str[i] != '\0'; i++)/*count string*/
+	{
+	}
+	newnode = malloc(sizeof(list_t));/*malloc for copy dest*/
+	if (str == NULL)
+		return (NULL);
+	newnode->str = strdup(str);/*copy string to newnode string*/
+	newnode->len = i;/*copy length string to newnode string*/
+	newnode->next = *head;/*copy adress of string to head*/
+	*head = newnode;/*head ni newnode wo dainyu*/
+	return (newnode);
+}
