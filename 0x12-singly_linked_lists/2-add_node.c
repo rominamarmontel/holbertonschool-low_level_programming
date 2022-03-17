5a466aaf4a09b20429e0e2b248e58a1fb391a53e
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-17 21:19:12 +0100

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,15 +11,15 @@ list_t *add_node(list_t **head, const char *str)
 	unsigned int i;
 	list_t *newnode; /*new node*/
 
-	for (i = 0; str[i] != '\0'; i++)/*count string*/
+	for (i = 0; str[i] != '\0'; i++) /*count string*/
 	{
 	}
-	newnode = malloc(sizeof(list_t));/*malloc for copy dest*/
-	if (str == NULL)
-		return (NULL);
-	newnode->str = strdup(str);/*copy string to newnode string*/
-	newnode->len = i;/*copy length string to newnode string*/
-	newnode->next = *head;/*copy adress of string to head*/
-	*head = newnode;/*head ni newnode wo dainyu*/
+	newnode = malloc(sizeof(list_t)); /*malloc for copy dest*/
+	if (newnode == NULL)
+		return (0);
+	newnode->str = strdup(str); /*copy string to newnode string*/
+	newnode->len = i; /*copy length string to newnode string*/
+	newnode->next = *head; /*copy adress of string to head*/
+	*head = newnode; /*head ni newnode wo dainyu*/
 	return (newnode);
 }
