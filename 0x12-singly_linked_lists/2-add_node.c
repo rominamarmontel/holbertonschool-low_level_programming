8ddcce05de8503dcfcec1d2ae91efa7675b84312
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-19 16:50:16 +0100

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,22 +7,22 @@
  */
 
 list_t *add_node(list_t **head, const char *str)
-{
+{/*adds a new node at the beginning of a list_t*/
 	unsigned int i;
-	list_t *newnode; /*new node*/
+	list_t *temp; /*new node*/
 
-	for (i = 0; str[i] != '\0'; i++) /*count string*/
+	for (i = 0; str[i] != '\0'; i++) /*count string before adding*/
 	{
 	}
-	newnode = malloc(sizeof(list_t)); /*malloc for copy dest*/
-	if (newnode == NULL)
+	temp = malloc(sizeof(list_t)); /*malloc for copy dest*/
+	if (temp == NULL)
 	{
-		free(newnode);
+		free(temp);
 		return (NULL);
 	}
-	newnode->str = strdup(str); /*copy string to newnode string*/
-	newnode->len = i; /*copy length string to newnode string*/
-	newnode->next = *head; /*copy adress of string to head*/
-	*head = newnode; /*head ni newnode wo dainyu*/
-	return (newnode);
+	temp->str = strdup(str);/*temp ni string of node wo dainyu*/
+	temp->len = i;/*temp ni length of node wo dainyu*/
+	temp->next = *head;/*temp ni adress of head wo dainyu*/
+	*head = temp;/*head ni temp wo dainyu*/
+	return (temp);
 }
