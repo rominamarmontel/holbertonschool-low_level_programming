eef43cf16f49323c564a261de6ecbf218bcf9381
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-22 10:06:12 +0100

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,18 +1,15 @@
 #include "lists.h"
-
 /**
  * free_listint2 - function sets the head to NULL
  * @head: first node
  * Return: rien
  */
-
 void free_listint2(listint_t **head)
 {
 	listint_t *newfree;
 
 	if (*head == NULL)
 		return;
-
 	while (*head)
 	{
 		newfree = (*head)->next;
