8c981f9d3a7bd39d210733f9ef61def283fc3ead
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-22 13:39:57 +0100

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
  * *get_nodeint_at_index - return the nth node
  * @head: first node
  * @index: param
- * Return:
+ * Return: nth
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
