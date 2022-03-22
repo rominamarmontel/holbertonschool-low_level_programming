207715b67399111974f5353529aa8cf8da0a497e
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-22 09:10:48 +0100

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,9 @@
 #include "lists.h"
-
 /**
  * print_listint - function for printing all the elements of linked list
  * @h: a pointer of head node
  * Return: the number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t i;
