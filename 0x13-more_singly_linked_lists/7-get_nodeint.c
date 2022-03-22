d5beefda67ad1e55e1773d86bc08006f9bcc463b
rominamarmontel <romi.varnier@gmail.com>
2022-03-22 13:36:56 +0100

    7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * *get_nodeint_at_index - return the nth node
+ * @head: first node
+ * @index: param
+ * Return:
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *nth = head;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		if (nth == NULL)
+			return (NULL);
+		nth = nth->next;
+	}
+	return (nth);
+}
