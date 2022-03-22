e57623fce8d2fdb1fca777237a0655af93f095f6
rominamarmontel <romi.varnier@gmail.com>
2022-03-22 11:10:58 +0100

    6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete the node
+ * @head: first node
+ * Return: the head node’s data (n)
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *newfree;
+	int i;
+	
+	if (head == NULL)
+	{
+	  return (0);
+	}
+	newfree = *head;
+	i = (*head)->n;
+	*head = (*head)->next;
+	free(newfree);
+	return (i);
+}
