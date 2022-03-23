029cea638d61275fbfc9cc7a25ae06e02f1eb264
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-23 09:39:17 +0100

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,12 +7,11 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t i;
-	listint_t *temp = h;
 
 	for (i = 0; h != NULL; i++)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	return (i);
 }
