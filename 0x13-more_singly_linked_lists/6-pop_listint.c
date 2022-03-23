93e8acec0d90a0ebdb7ee0eb722e1a800b307d95
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-23 20:04:12 +0100

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,16 +8,17 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t *newfree;
+	listint_t *temp;
 	int i;
 
 	if (*head == NULL)
 	{
 		return (0);
 	}
-	newfree = *head;
-	i = (*head)->n;
-	*head = (*head)->next;
-	free(newfree);
+	temp = *head;/*temp wo head ni awaseru*/
+	i = (*head)->n;/*i ni head no  wo ireru*/
+	temp = (*head)->next;/*temp ni head no adress wo ireru*/
+	free(*head);/*head wo sakujo*/
+	*head = temp;/*temp wo head ni ireru*/
 	return (i);
 }
