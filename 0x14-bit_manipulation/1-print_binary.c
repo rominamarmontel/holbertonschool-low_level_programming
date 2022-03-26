8bd7ab46c017f3c6589d3b7262ba95dbf84ecd7b
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-26 12:45:36 +0100

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,6 +8,6 @@
 void print_binary(unsigned long int n)
 {
 	if (n > 1)/*decimal ga 2 ijo no baai, soreika wa sonomama no kazu*/
-		print_binary (n >> 1); /*migi ni 1 zutsu zurasu*/
+		print_binary(n >> 1); /*migi ni 1 zutsu zurasu*/
 	_putchar((n & 1) + '0');/*saigo kara hyoji*/
 }
