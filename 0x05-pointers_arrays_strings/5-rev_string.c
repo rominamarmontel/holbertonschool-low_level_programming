3318351b94b7c566a1934248c0cf40e7eddbe788
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-27 23:31:57 +0200

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,18 +6,16 @@
  */
 void rev_string(char *s)
 {
-	int i = 0;
-	int len = 0;
+	int i;
+	int j;
 	int temp;
 
-	while (s[len] != '\0')/*count size of string */
+	for (i = 0; s[i] != '\0'; i++)/*count size of string*/
+		;
+	for (j = 0; j < i / 2; j++)/*count half size of string*/
 	{
-		len++;
-	}
-	for (i = 0; i < len / 2; i++) /* count loop number with len/2 */
-	{
-		temp = s[i]; /* temp ni s[0]=H wo dainyu */
-		s[i] = s[len - i - 1]; /* s[0]=5-0-1=4=L wo dainyu */
-		s[len - i - 1] = temp; /* s=4 ni temp=H wo dainyu */
+		temp = s[j];/* temp ni s[0]=H wo dainyu */
+		s[j] = s[i - j - 1]; /* s[0]=5-0-1=4=L wo dainyu */
+		s[i - j - 1] = temp;/* s=4 ni temp=H wo dainyu */
 	}
 }
