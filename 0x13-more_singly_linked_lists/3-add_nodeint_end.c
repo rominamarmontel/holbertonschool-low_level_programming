27a65fdeb65098a9351be565bac0ea3eb7697c23
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-27 18:55:34 +0200

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,28 +9,27 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
-	listint_t *temp;
+	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 	new->n = n;
 	new->next = NULL;
-
 	if (*head == NULL)
 	{
 		*head = new;
 	}
 	else
 	{
-		temp = *head;
 		while (temp->next != NULL)
 		{
+			if (temp == NULL)
+				return (NULL);
 			temp = temp->next;
 		}
 		temp->next = new;
+		temp = new;
 	}
 	return (new);
 }
