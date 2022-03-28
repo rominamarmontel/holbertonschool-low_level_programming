7d537ee21eb1afbbd32b3997435b4777553243c2
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-28 16:57:33 +0200

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 			;
