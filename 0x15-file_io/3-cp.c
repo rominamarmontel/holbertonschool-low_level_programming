31dcea74dcad24b0f66c1db864ecb69f45b620a3
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-28 20:41:58 +0200

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,21 +10,21 @@
 int main(int argc, char *argv[])
 {
 	char buffer[1024];
-	int file_from, file_to, close_from, close_to;
+	int fdfrom, fdto, clofrom, cloto;
 	int wt, rd;
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+	fdfrom = open(argv[1], O_RDONLY);
+	if (fdfrom == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	exit(98);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to == -1)
+	fdto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fdto == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while ((rd = read(file_from, buffer, 1024)) > 0)
+	while ((rd = read(fdfrom, buffer, 1024)) > 0)
 	{
-		wt = write(file_to, buffer, rd);
+		wt = write(fdto, buffer, rd);
 		if (wt == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
@@ -32,11 +32,11 @@ int main(int argc, char *argv[])
 	if (rd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	exit(98);
-	close_from = close(file_from);
-	if (close_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
-	close_to = close(file_to);
-	if (close_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+	clofrom = close(fdfrom);
+	if (clofrom == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdfrom), exit(100);
+	cloto = close(fdto);
+	if (cloto == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdto), exit(100);
 	return (0);
 }
