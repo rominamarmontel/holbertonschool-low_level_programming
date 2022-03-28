3f0279eabb749d14d7ffad6232a2f7a4bae62e76
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-28 16:52:59 +0200

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_file - create a file
+ * @filename: filename
+ * @text_content: text
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i;
+	int wt = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		wt = write(fd, text_content, i);
+	}
+	if (wt == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
