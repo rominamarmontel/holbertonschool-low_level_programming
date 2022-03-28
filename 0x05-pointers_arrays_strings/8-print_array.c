a55568432f7d9eae09dd1ad83a691e8228417619
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-28 14:06:29 +0200

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; a[i] != '\0' && i < n; i++)
 	{
 		printf("%d", a[i]);
 		if (i != n - 1)
-		{
 			printf(", ");
-		}
 	}
 	printf("\n");
 }
