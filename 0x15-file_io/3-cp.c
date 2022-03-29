f694d5537bef5ebeb738c1f2f1251101e44974e2
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-03-29 10:35:50 +0200

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,25 +10,25 @@
 
 int main(int argc, char *argv[])
 {
-	int openfrom, opento, r, w, closeto, closefrom;
-	char str[1024];
+	int fdfrom, fdto, r, w, clto, clfrom;
+	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	openfrom = open(argv[1], O_RDONLY);
-	if (openfrom == -1)
+	fdfrom = open(argv[1], O_RDONLY);/*file wo hiraku*/
+	if (fdfrom == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	opento = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (opento == -1)
+	fdto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fdto == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while ((r = read(openfrom, str, 1024)) > 0)
+	while ((r = read(fdfrom, buffer, 1024)) > 0)
 	{
-		w = write(opento, str, r);
+		w = write(fdto, buffer, r);
 		if (w == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
@@ -38,13 +38,13 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	closefrom = close(openfrom);
-	if (closefrom == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", openfrom), exit(100);
+	clfrom = close(fdfrom);
+	if (clfrom == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdfrom), exit(100);
 
-	closeto = close(opento);
-	if (closeto == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", opento), exit(100);
+	clto = close(fdto);
+	if (clto == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdto), exit(100);
 
 	return (0);
 }
