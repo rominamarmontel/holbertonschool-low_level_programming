a7b856d5dfb94faf305c2bf0b6d483bcb4042ec5
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-04-18 15:52:41 +0200

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,13 +7,16 @@
 
 int _isalpha(int c)
 {
-	if ((c < 123 && c > 96) || (c < 91 && c > 64))
+	char min;
+	char maj;
+
+	for (min = 'a'; min <= 'z'; min++)
 	{
-		return (1);
+		for (maj = 'A'; maj <= 'Z'; maj++)
+		{
+			if (c == min || c == maj)
+			return (1);
+		}
 	}
-	else
-	{
-		return (0);
-	}
-	_putchar('\n');
+	return (0);
 }
