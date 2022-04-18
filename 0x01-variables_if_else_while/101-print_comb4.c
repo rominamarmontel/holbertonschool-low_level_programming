572678c2c485c5669e46b7081d5d7ba7f80d2f13
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-04-18 15:01:32 +0200

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,26 +10,30 @@
 
 int main(void)
 {
-	int a;
-	int b;
-	int c;
+	int i;
+	int j;
+	int k;
 
-	for (a = '0'; a <= '7'; a++)
-		for (b = '1'; b <= '8'; b++)
-			for (c = '2'; c <= '9'; c++)
-				if (a != b && b != c && c != a)
-				if (a < b && b < c)
+	for (i = '0'; i <= '7'; i++)
+	{
+		for (j = '0'; j <= '8'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (i < j && j < k)
 				{
-					putchar(a);
-					putchar(b);
-					putchar(c);
-
-					if (a != '7' || b != '8' || c != '9')
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != '7' || j != '8' || k != '9')
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
