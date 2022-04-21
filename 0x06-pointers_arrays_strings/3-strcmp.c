fa43ca7887b4ad7fd63d43c955a85f7d571135f0
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-04-21 16:54:00 +0200

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+	int j;
 
-	for (i = 0; s1[i] != '\0'; i++)/*count size of s1*/
+	for (i = 0; s1[i] != '\0'; i++)
+	;
+	for (j = 0; s2[j] != '\0'; j++)
 	{
+		if (s1[0] != s2[0])
+		return (s1[0] - s2[0]);
 	}
-	for (i = 0; s2[i] != '\0'; i++)/*count size of s2*/
-	{
-		if (s1[i] != s2[i])/*if different size */
-		{
-			return (s1[i] - s2[i]);/*ASCII 72-87*/
-		}
-	}
-	return (0);/*if the same*/
+	return (0);
 }
