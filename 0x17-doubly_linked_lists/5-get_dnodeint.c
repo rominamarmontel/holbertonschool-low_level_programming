8ff23522e839095fe76a1eb46111d305001bfccc
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-05 19:10:08 +0200

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -2,25 +2,23 @@
 /**
  * *get_dnodeint_at_index - free node
  * @head: param
+ * @index: param
  * Return: Number of node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *temp = *head;
-	dlistint_t *new;
+	dlistint_t *temp = head;
 	unsigned int i;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return NULL;
-	if (*head == NULL)
+	if (head == NULL)
 	{
-		new->prev = NULL;
-		*head = new;
-		return (new);
-	}	
+		return (NULL);
+	}
 	for (i = 0; i < index; i++)
 	{
+		if (temp == NULL)
+			return (NULL);
 		temp = temp->next;
 	}
+	return (temp);
 }
