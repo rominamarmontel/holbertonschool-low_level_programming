f685a974bc24d6a10a69a92d5ea873e1f1c45e9a
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-06 10:45:42 +0200

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 /**
+ * dlistint_len - function
+ * @h: param
+ * Return: Number of node
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+
+}
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: param
  * @idx: param
@@ -11,7 +30,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = *h;
 	dlistint_t *new, *nx_idx;
 	unsigned int i = 0;
+	size_t len = dlistint_len(*h);
 
+	if (*h == NULL || idx == 0)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
+	if (len < idx)
+		return (NULL);
+	if (idx == len)
+	{
+		new = add_dnodeint_end(h, n);
+		return (new);
+	}
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
