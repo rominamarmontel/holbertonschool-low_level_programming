a70390f67ba0f96973f9bec01c4466308318b1c0
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-06 10:23:42 +0200

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *nx_idx;
 	unsigned int i = 0;
 
+	if (*h == NULL)
+	{
+		h = &new;
+	        return (new);
+	}
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
