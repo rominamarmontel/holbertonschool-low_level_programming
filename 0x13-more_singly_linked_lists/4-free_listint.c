51d54386599d69dfd59b4a5ed3001e85d38cec0a
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-07 17:23:29 +0200

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,11 +7,13 @@
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
-	while(head)
+
+	while (head)
 	{
 		temp = head;
 		temp = head->next;
 		free(head);
 		head = temp;
 	}
+	free(temp);
 }
