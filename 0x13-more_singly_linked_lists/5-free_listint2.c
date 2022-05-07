0bf02148adad883f1574754a691f4525e360a87e
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-07 17:33:03 +0200

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
 
-	if (*head == NULL)
-		return;
 	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
+	head = NULL;
 }
