4b924a931a8d14d94f6bfb7cebe64ec636a10627
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-08 18:12:24 +0200

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -6,15 +6,14 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp = head;
 	int sum = 0;
 
 	if (head == NULL)
 		return (0);
-	while (temp)
+	while (head)
 	{
-		sum += temp->n;
-		temp = temp->next;
+		sum += head->n;
+		head = head->next;
 	}
 	return (sum);
 }
