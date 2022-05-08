110fa9d0654c9db6f95811400f3c886d7210dfad
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-08 10:54:23 +0200

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,13 +8,14 @@ int sum_listint(listint_t *head)
 {
 	listint_t *temp = head;
 	int i;
+	int sum = 0;
 
 	if (head == NULL)
 		return (0);
-	for (i = 0; temp != NULL;)
+	for (i = 0; temp->next != NULL; i++)
 	{
-		i += temp->n;/*temp no deata wo i kai tasu*/
-		temp = temp->next;/*temp wo susumeru*/
+		temp = temp->next;
+		sum += temp->n;
 	}
-	return (i);
+	return (sum);
 }
