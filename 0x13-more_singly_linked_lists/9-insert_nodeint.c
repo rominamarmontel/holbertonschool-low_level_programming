0dc749006a371ee8ccbd9826f4d2e7fb595ffef2
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-08 11:27:12 +0200

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,27 +13,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp = *head;
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = n; /*new no data ni n wo ireru*/
-	if (idx == 0) /*index ga 0 no baai*/
+	new->n = n;
+	if (idx == 0)
+		next->next = *head;
+		*head = new;
+	for (i = 0; i < idx - 1; i++)
 	{
-		new->next = *head; /*new->next ni head no nakami wo ireru*/
-		*head = new; /*head ni new no adress wo ireru*/
+		if (temp == NULL)
+			return (NULL);
+		temp = temp->next;
 	}
-	else
-	{
-		for (i = 1; i < idx; i++) /*i ga 1kara idx made incriment*/
-		{
-			if (temp->next == NULL)/*node no tsuika ga damenara*/
-				return  (NULL);
-			temp = temp->next;/*temp wo susumeru*/
-		}
-		new->next = temp->next;
-		temp->next = new;/*temp->next ni new no adress wo ireru*/
-	}
+	new->next = temp->next;
+	temp->next = new;
+	new = temp;
 	return (new);
 }
