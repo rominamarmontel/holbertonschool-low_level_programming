654d4b6a8c489282e4072da29275f96aa3e94679
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-08 17:59:51 +0200

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,18 +7,13 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *temp = head;
 	unsigned int i;
 
 	if (head == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < index; i++)
 	{
-		if (temp == NULL)
-			return (NULL);
-		temp = temp->next;
+		head = head->next;
 	}
-	return (temp);
+	return (head);
 }
