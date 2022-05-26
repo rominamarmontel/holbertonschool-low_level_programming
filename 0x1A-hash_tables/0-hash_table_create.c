4cca8a3b098cad403ef8e0b1b4a1fb8357729a98
romi.varnier@gmail.com <romi.varnier@gmail.com>
2022-05-26 18:21:09 +0200

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create a hash table
+ * @size: size of array
+ * Return: new hash table, NULL if failed
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new;
+
+	if (size < 1)
+		return (NULL);
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t *) * new->size);
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	return (new);
+}
