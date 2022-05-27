e3f0c748e337bbec6a419f8bf87390248f283598
Romi <romi@MacBook-Pro-de-Romi.local>
2022-05-27 21:20:02 +0200

    third

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 
-	if (size == 0)
+	if (size == NULL)
 		return (NULL);
 	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
