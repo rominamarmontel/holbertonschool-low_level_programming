b0872c72bf0fcf9f81ae3044df1c12eb65f5bd81
Romi <romi@MacBook-Pro-de-Romi.local>
2022-05-27 21:49:18 +0200

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - create a has
- * @size: size of array
- * Return: new has, NULL if failed
+ * hash_table_create - create a hash table
+ * @size: size of the array
+ * Return: hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
