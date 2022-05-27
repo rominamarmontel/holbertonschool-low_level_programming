d431c91c056ce95e6eebcc1bc722c5f1a7351b32
Romi <romi@MacBook-Pro-de-Romi.local>
2022-05-27 21:18:42 +0200

    second

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,17 +8,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 
+	if (size == 0)
+		return (NULL);
 	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
-	{
-		free(new);
 		return (NULL);
-	}
 	new->size = size;
 	new->array = malloc(sizeof(hash_node_t *) * size);
 	if (new->array == NULL)
 	{
-		free(new->array);
+		free(new);
 		return (NULL);
 	}
 	return (new);
