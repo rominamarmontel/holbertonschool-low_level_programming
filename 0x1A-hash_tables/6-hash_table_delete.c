6111c564385373b02a801890b894e0d00221df84
Romi <romi@MacBook-Pro-de-Romi.local>
2022-05-27 23:24:54 +0200

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,22 +6,22 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *temp;
-	unsigned long int index;
+hash_node_t *temp;
+unsigned long int index;
 
-	if (ht == NULL)
-		return;
-	for (index = 0; index < ht->size; index++)
-	{
-		while (ht->array[index] != NULL)
-		{
-			temp = ht->array[index]->next;
-			free(ht->array[index]->key);
-			free(ht->array[index]->value);
-			free(ht->array[index]);
-			ht->array[index] = temp;
-		}
-	}
-	free(ht->array);
-	free(ht);
+if (ht == NULL)
+return;
+for (index = 0; index < ht->size; index++)
+{
+while (ht->array[index] != NULL)
+{
+temp = ht->array[index]->next;
+free(ht->array[index]->key);
+free(ht->array[index]->value);
+free(ht->array[index]);
+ht->array[index] = temp;
+}
+}
+free(ht->array);
+free(ht);
 }
