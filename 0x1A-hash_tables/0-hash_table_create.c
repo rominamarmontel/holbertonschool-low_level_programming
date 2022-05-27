3d1566b50fcd856c9d85847b4dc7433e8828933a
Romi <romi@MacBook-Pro-de-Romi.local>
2022-05-27 23:15:59 +0200

    6-hash_table_delete.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,19 +6,17 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new = NULL;
+hash_table_t *new = NULL;
 
-	new = malloc(sizeof(hash_table_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->size = size;
-
-	new->array = malloc(sizeof(hash_node_t *) * size);
-	if (new->array == NULL)
-	{
-		free(new);
-		return (NULL);
-	}	
-	return (new);
+new = malloc(sizeof(hash_table_t));
+if (new == NULL)
+return (NULL);
+new->size = size;
+new->array = malloc(sizeof(hash_node_t *) * size);
+if (new->array == NULL)
+{
+free(new);
+return (NULL);
+}
+return (new);
 }
