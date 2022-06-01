5a8cded9039f08c1b7d2393c37ed914dee2928bf
Romi <romi@MacBook-Pro-de-Romi.local>
2022-06-01 10:51:15 +0200

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,22 +6,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-hash_node_t *temp;
-unsigned long int index;
-
-if (ht == NULL)
-return;
-for (index = 0; index < ht->size; index++)
-{
-while (ht->array[index] != NULL)
-{
-temp = ht->array[index]->next;
-free(ht->array[index]->key);
-free(ht->array[index]->value);
-free(ht->array[index]);
-ht->array[index] = temp;
-}
-}
-free(ht->array);
-free(ht);
+	hash_node_t *temp;
+	hash_node_t *head;
+	unsigned long int index;
+	
+	if (ht == NULL)
+		return;
+	for (index = 0; index < ht->size; index++)
+	{
+		head = ht->array[index];
+		while (head != NULL)
+		{
+			temp = head->next;
+			free(head->key);
+			free(head->value);
+			free(head);
+			head = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
