f7dc3c0877cf178f2043904b3a024940fe2f0e62
rominamarmontel <romi.varnier@gmail.com>
2022-07-15 16:54:33 +0200

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""This module defines the function island_perimeter"""
+"""Create a function def island_perimeter(grid)"""
 
 
 def island_perimeter(grid):
-    """This module defines the function island_perimeter"""
+    """This module returns the perimeter of the island described in grid"""
     perimetre = 0
     for col in range(0, 5):
-        for row in range(0, 6):
+        for row in range(len(grid)):
             if grid[col][row] == 1:
                 if grid[col - 1][row] != 1:
                     perimetre += 1
