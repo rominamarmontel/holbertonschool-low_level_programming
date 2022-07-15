db75561bad393e3f2503e8adbf982561bc79db6e
rominamarmontel <romi.varnier@gmail.com>
2022-07-15 20:52:05 +0200

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,7 +6,7 @@ def island_perimeter(grid):
     """This module returns the perimeter of the island described in grid"""
     perimetre = 0
     for yoko in range(len(grid)):
-        for tate in range(len(grid[tate])):
+        for tate in range(len(grid[yoko])):
             if grid[yoko][tate] == 1:
                 if grid[yoko - 1][tate] != 1:
                     perimetre += 1
